34d0477b49b129a62865802e5d2c3f2012081740
bazz <bazuaye50@gmail.com>
2024-09-03 08:20:28 +0100

    update

---
+++ donice.py
@@ -0,0 +1,32 @@
+donice = 20
+
+def Hello(self, parameter_list):
+    
+    print(donice)
+
+    pass
+
+bazz_arr = [1, 2, 3,4 , 5,6 ]
+
+bazz_dict = [
+    {
+    "name": "prick", 
+    "id": 1
+},
+    {
+    "name": "prick", 
+    "id": 1
+},
+    {
+    "name": "prick", 
+    "id": 1
+},
+    {
+    "name": "prick", 
+    "id": 1
+},
+]
+
+
+for i in bazz_arr:
+    print("loop",i)
--- excercise/arrays/one.py
+++ excercise/arrays/one.py
@@ -1,12 +1,5 @@
 months = [2200, 2350,2600,2130,2190, 2000]
 
-
-def check_extra_cost(month):
-    """
-    docstring
-    """
-    pass
-
 print("Extra cost feb - jan",(months[0] - months[1]* 1))
 print("Sum first quater", sum(months[:3]))
 
@@ -16,5 +9,7 @@ print('spent_2000k: ', spent_2000k)
 
 months.append(1980)
 print(months)
-print( months[3] - 200)
+months[3] = months[3] - 200
+print('months: ', months)
+
 print ("2000 in any month", 2000 in months)
